7680ba30720e2d7f5a70b9fb6172e9dd46790933
midbel <brun.nicolas21@gmail.com>
2022-01-21 19:49:58 +0100

    rewrite advance

--- wrap.go
+++ wrap.go
@@ -15,7 +15,7 @@ func Shorten(str string, n int) string {
 	if n <= 0 || n >= len(str) {
 		return str
 	}
-	str, _ = advance(str, n)
+	str, _, _ = advance(str, n)
 	return fmt.Sprintf("%s...", str)
 }
 
@@ -23,8 +23,8 @@ func Wrap(str string) string {
 	return WrapN(str, DefaultLength)
 }
 
-func WrapN(str string, n int) string {
-	if n <= 0 {
+func WrapN(str string, limit int) string {
+	if limit <= 0 {
 		return str
 	}
 	var (
@@ -32,74 +32,97 @@ func WrapN(str string, n int) string {
 		ptr int
 	)
 	for i := 0; ptr < len(str); i++ {
-		if i > 0 {
+		next, x, addnl := advance(str[ptr:], limit)
+		if i > 0 && ptr < len(str) && x > 1 {
 			ws.WriteRune(nl)
 		}
-		next, x := advance(str[ptr:], n)
-		if x == 0 {
+		ptr += x
+		ws.WriteString(next)
+		if addnl && len(next) > 0 {
+			ws.WriteRune(nl)
+		}
+		if x == 0 || ptr >= len(str) {
 			break
 		}
-		ws.WriteString(strings.TrimSpace(next))
-		ptr += x
 	}
 	return ws.String()
 }
 
-func advance(str string, n int) (string, int) {
+func advance(str string, limit int) (string, int, bool) {
 	if len(str) == 0 {
-		return "", 0
+		return "", 0, false
 	}
 	var (
-		curr int
 		prev int
-		ws   strings.Builder
+		curr int
+		step int
+		last rune
+		ws strings.Builder
 	)
+	curr += skip(str[curr:], isBlank)
 	for {
-		r, z := utf8.DecodeRuneInString(str[curr:])
-		if r != utf8.RuneError {
-			curr += z
+		last, step = peek(str[curr:], &ws)
+		if last == utf8.RuneError {
+			return ws.String(), curr, false
 		}
-		if isNL(r) {
-			ws.WriteRune(nl)
-			curr += skip(str[curr:], isNL)
+		curr += step
+		if isNL(last) {
+			step = skip(str[curr:], isNL)
+			return ws.String(), curr+step, step > 0 && len(str[curr+step:]) > 0
+		}
+		ws.WriteRune(last)
+		if n, ok := canBreak(curr, prev, limit); ok {
+			curr = n
 			break
 		}
-		if isDelimiter(r) {
-			if isBlank(r) {
-				curr += skip(str[curr:], isBlank)
-			}
-			if z, ok := canBreak(ws.Len(), prev, n); ok {
-				curr = z
-				if isPunct(r) {
-					ws.WriteRune(r)
-				}
-				break
-			}
-			prev = curr
-		}
-		if r == utf8.RuneError {
-			break
-		}
-		ws.WriteRune(r)
+		prev = curr
 	}
-	str = ws.String()
-	if z := len(str); z > curr {
+	if str = ws.String(); len(str) > curr {
 		str = str[:curr]
 	}
-	return str, curr
+	return str, curr, false
+}
+
+func peek(str string, ws *strings.Builder) (rune, int) {
+	var (
+		curr int
+		size int
+		last rune
+	)
+	for {
+		last, size = next(str[curr:])
+		curr += size
+		if last == utf8.RuneError {
+			return last, curr
+		}
+		if last == backslash {
+			r, x := next(str[curr:])
+			if isNL(r) {
+				curr += x
+				continue
+			}
+		}
+		if isDelimiter(last) {
+			break
+		}
+		ws.WriteRune(last)
+	}
+	return last, curr
+}
+
+func next(str string) (rune, int) {
+	return utf8.DecodeRuneInString(str)
 }
 
 func canBreak(curr, prev, limit int) (int, bool) {
-	if curr == limit {
-		return curr, true
+	if curr < limit {
+		return 0, false
 	}
-	if curr > limit && curr-limit < DefaultThreshold {
-		return curr, true
+	delta := limit - prev
+	if diff := curr - limit; delta < diff {
+		curr = prev
 	}
-	if curr > limit && curr-limit > DefaultThreshold {
-		return prev, true
-	}
-	return 0, false
+	return curr, true
 }
 
 func skip(str string, fn func(rune) bool) int {
@@ -124,10 +147,11 @@ const (
 	bang      = '!'
 	colon     = ':'
 	semicolon = ';'
+	backslash = '\\'
 )
 
 func isDelimiter(r rune) bool {
-	return isPunct(r) || isBlank(r) || r == utf8.RuneError
+	return isNL(r) || isPunct(r) || isBlank(r) || r == utf8.RuneError
 }
 
 func isPunct(r rune) bool {
